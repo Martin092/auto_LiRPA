Fixed issues with the hessian graph creation, and other issues that arose from commit 21861db

auto_LiRPA/operators/activations.py
@@ -62,7 +62,12 @@ class BoundSoftplus(BoundActivation):
         return [(grad_node, grad_input, [self.inputs[0]])]
 
     def build_hessian_node(self, grad_upstream, hessian_upstream):
-        hessian_node = SoftplusHessian(beta=self.softplus.beta)
+        preact = self.inputs[0].forward_value
+        hessian_node = SoftplusHessian(
+            beta=self.softplus.beta,
+            dim=preact.shape[-1],
+            dtype=preact.dtype,
+            device=preact.device)
         hessian_input = (grad_upstream, hessian_upstream, self.inputs[0].forward_value)
         return [(hessian_node, hessian_input, [self.inputs[0]])]
 
@@ -79,17 +84,19 @@ class SoftplusGrad(Module):
 
 
 class SoftplusHessian(Module):
-    def __init__(self, beta=1.0):
+    def __init__(self, beta=1.0, dim=None, dtype=None, device=None):
         super().__init__()
         self.beta = beta
+        self.register_buffer(
+            'eye', torch.eye(dim, dtype=dtype, device=device))
 
     def forward(self, grad_last, hessian_last, preact):
         d1 = torch.sigmoid(self.beta * preact)
         d2 = self.beta * d1 * (1 - d1)
         grad_input = grad_last * d1.unsqueeze(1)
         scale = d1.unsqueeze(1).unsqueeze(-1) * d1.unsqueeze(1).unsqueeze(-2)
-        hessian_input = hessian_last * scale
-        hessian_input.diagonal(dim1=-2, dim2=-1).add_(grad_last * d2.unsqueeze(1))
+        diagonal_term = (grad_last * d2.unsqueeze(1)).unsqueeze(-1) * self.eye
+        hessian_input = hessian_last * scale + diagonal_term
         return grad_input, hessian_input
 
 

auto_LiRPA/operators/hessian.py
@@ -42,7 +42,8 @@ class BoundHessianInit(Bound):
         self.no_jacobian = True
 
     def forward(self, x, y):
-        return torch.zeros(x.shape[0], *y.shape[1:], *x.shape[1:], *x.shape[1:])
+        return x.new_zeros(
+            x.shape[0], *y.shape[1:], *x.shape[1:], *x.shape[1:])
 
 class BoundHessianOP(Bound):
     def __init__(self, attr=None, inputs=None, output_index=0, options=None):

auto_LiRPA/operators/linear.py
@@ -954,9 +954,24 @@ class BoundLinear(BoundOptimizableActivation):
                 "Gradient computation for weight perturbation is not supported yet.")
 
     def build_hessian_node(self, grad_upstream, hessian_upstream):
-        grad_node = LinearHessianProp()
-        grad_input = (grad_upstream, hessian_upstream, self.inputs[1].forward_value)
-        return [(grad_node, grad_input, [self.inputs[0], self.inputs[1]])]
+        if not self.is_input_perturbed(1):
+            if isinstance(self.inputs[1], BoundParams):
+                w = self.inputs[1].param
+            elif isinstance(self.inputs[1], BoundBuffers):
+                w = self.inputs[1].buffer
+            else:
+                w = self.inputs[1].value
+            if not self.transB:
+                w = w.t()
+            grad_node = LinearHessianProp(w.detach())
+            grad_input = (grad_upstream, hessian_upstream)
+            return [(grad_node, grad_input, [])]
+        else:
+            raise NotImplementedError(
+                "Hessian computation for weight perturbation is not supported yet.")
+
+    def update_requires_input_bounds(self):
+        self._check_weight_perturbation()
 
 
 class BoundMatMul(BoundLinear):
@@ -1125,7 +1140,12 @@ class LinearHessian(Module):
         return torch.zeros(self.out_dim, self.input_dim, self.input_dim)
 
 class LinearHessianProp(Module):
-    def forward(self, grad_upstream, hessian_upstream, weight):
+    def __init__(self, weight):
+        super().__init__()
+        self.weight = weight
+
+    def forward(self, grad_upstream, hessian_upstream):
+        weight = self.weight.to(grad_upstream)
         grad_input = grad_upstream @ weight
         hessian_input = weight.T @ hessian_upstream @ weight
         return grad_input, hessian_input